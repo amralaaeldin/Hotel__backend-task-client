0965af27fb3f04c016b194d1b311bbab3bfb548a
Amr Alaaeldin <amralaaeldin.dev@gmail.com>
2022-11-28 15:33:11 +0200

    remove dead comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import Hotel from './Hotel';
-// import '@fontsource/roboto/300.css';
-// import '@fontsource/roboto/400.css';
-// import '@fontsource/roboto/500.css';
-// import '@fontsource/roboto/700.css';
 import './styles/App.css';
 
 function App() {
